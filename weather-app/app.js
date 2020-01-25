852afef748b92d3625652a22a4415e9b7cbbe0ad
cdsalty <soltiscd@gmail.com>
2020-01-24 23:45:45 -0500

    Callback Abstractions

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request'); // see 'all available options' on npm
 
+/*
 const url =
   'https://api.darksky.net/forecast/5e8a54e1354605908f9c1d425506dea9/37.8267,-122.4233';
 const spainishUrl =
@@ -75,6 +76,8 @@ request({url: geoCodeUrl, json: true}, (error, response) => {
  ** ** CALLBACKS ** **
 Goal: Use the callback pattern
 
+EXERCISE D:
+
 1. Define an add function that accepts the correct arguments
 2. Use setTimeout to simulate a 2 second delay
 3. After 2 seconds are up, call the callback function with the sum
@@ -84,16 +87,56 @@ Goal: Use the callback pattern
 //   console.log(sum); // Should print: 5
 // });
 
-// thinking... add takes in 3 arguements, 2 numbers and a callback fumction call 'sum'
+// thinking... add takes in 3 arguements, 2 numbers and a "callbackFunction" function that later will be
+// referenced with by calling calling 'sum'
 
-// finally...
-const add = (x, y, callback) => {
-  //done
+const add = (x, y, callbackFunction) => {
   setTimeout(() => {
     // queue
-    callback(x + y); // this will not run until 3 secs passes, callback(x+y)
+    callbackFunction(x + y); // this will not run until 3 secs passes, callback(x+y)
   }, 3000);
 };
-add(1, 4, (sum) => {
-  console.log(sum); // Should print: 5
+// add(1, 4, (sum) => {
+//   // sum represents the 'callback' function
+//   console.log(sum); // Should print: 5
+// });
+
+/* 
+EXERCISE D:
+
+Callback Challenge:
+- Using both apis, first get the address(lat and long). Then use that information to retrieve the current
+  location's weather; Once I have the latitude and longitude back, I will then use a callback that I referenced
+  as 'callback'
+- encodeURIComponent(address); may be required... 
+
+*/
+const geoCode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiY2Rzb2x0aXMiLCJhIjoiY2s1b3Nwb2N6MGFnajNscGNlMG9yajFjNiJ9.Tx0JlVZRjDuAsvb7xSfiMg`;
+
+  request({url: url, json: true}, (error, response) => {
+    // write an 'if' statement that will determine how things went. if that went well or not....f
+    if (error) {
+      // make first reference to our 'callback' if there is an error;
+      callback('unable to connect to local server', undefined);
+    } else if (response.body.features.length === 0) {
+      // if I don't get any search results, there is a problem.
+      callback('unable to find location, try again', undefined);
+    } else {
+      callback(undefined, {
+        // this should be sent back as the data;
+        latitude: response.body.features[0].center[0],
+        longitude: response.body.features[0].center[1],
+        location: response.body.features[0].place_name
+      });
+    }
+  });
+};
+// - geoCode will communicate with the mapbox api
+// callback functions will give you an error if things go poorly or data if we get our response.
+geoCode('Atlanta', (error, data) => {
+  console.log('error', error);
+  console.log('data:', data);
 });
+
+// the callback expects two things, the error(if there is one) otherwise, the data.
