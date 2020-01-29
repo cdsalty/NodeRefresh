773ac0265b7c0cb6ad068d36051487acef90a03f
cdsalty <soltiscd@gmail.com>
2020-01-29 12:05:48 -0500

    making use of partials for header footer

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,24 @@
 const path = require('path'); // used to concate the directory path along with the path I intend to go too
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express();
+
+/*
+Challenge: Create a partial for the footer
+  1. Setup the template for the footer partial with a paragraph: "Created by .... "
+  2. Render the partial at the bottom of all three pages
+  3. Test by visiting all pages
+*/
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public'); // calling the src destination + the public url info
 const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPaTH = path.join(__dirname, '../templates/partials');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs'); // in short, telling express we're using a templating engine.
 app.set('views', viewsPath); // replacing default folder name of views with the viewsPath directory
+hbs.registerPartials(partialsPath); // to use partials
 
 // Setup Static Directory to serve
 app.use(express.static(publicDirectory)); // static is an express method that calls on use the method for url routing
@@ -26,15 +34,17 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
   // small mistake not to make that cost me time './about' vs '/about'
   res.render('about', {
-    from: 'Georgia',
+    title: 'About',
+    name: 'Christopher',
     pupsName: 'Daisy'
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    phoneContactName: 'Ellie',
-    phone: '876-501-3905'
+    helpText: 'This is some HELPful text',
+    title: 'Help',
+    name: 'Christopher'
   });
 });
 
