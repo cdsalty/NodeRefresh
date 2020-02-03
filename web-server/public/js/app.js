d8516c8777a06006e431464dbed8549020f44dce
cdsalty <soltiscd@gmail.com>
2020-02-03 18:11:58 -0500

    added error if error

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -8,6 +8,12 @@ Challenge: Fetch The Weather
 */
 fetch('http://localhost:3000/weather?address=atlanta').then((response) => {
   response.json().then((data) => {
-    console.log(data);
+    // console.log(data);
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
   });
 });
